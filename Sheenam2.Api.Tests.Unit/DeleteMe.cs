b8afe1870634212f4fbe22e8be86f23fb950e30a
Azimjon <azimjon.007@mail.ru>
2023-01-29 09:18:47 +0200

    INFRA:Initialize Unit Test Project

---
+++ Sheenam2.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free To Use To Find Confort and Peace
+//=================================================
+
+using Xunit;
+
+namespace Sheenam2.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
