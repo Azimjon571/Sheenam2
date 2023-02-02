ae18be28631317bbc87dd106f59c849b6c08eaec
Azimjon <azimjon.007@mail.ru>
2023-02-02 08:04:13 +0200

    DATA: Model & Migrate Guests

---
+++ Sheenam2.API/Brokers/Storages/StorageBroker.Guests.cs
@@ -0,0 +1,17 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free To Use To Find Confort and Peace
+//=================================================
+
+
+using Microsoft.EntityFrameworkCore;
+using Sheenam2.API.Models.Foundation.Guests;
+
+namespace Sheenam2.API.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Guest> Guests{get;set;}
+
+    }
+}
---
+++ Sheenam2.API/Models/Foundation/Guests/GenderType.cs
@@ -0,0 +1,14 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free To Use To Find Confort and Peace
+//=================================================
+
+namespace Sheenam2.API.Models.Foundation.Guests
+{
+    public enum GenderType
+    {
+        Male,
+        Female,
+        Other
+    }
+}
